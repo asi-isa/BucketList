c3b0f2d10a4c7c3c3ed92d76a85fd50c411e9487
asi-isa <ali.inceoglu@protonmail.com>
2025-12-07 23:07:05 +0100

    Read and Write to file

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Read and Write") {
+                let data = Data("Hello, World!".utf8)
+                let url = URL.applicationDirectory.appending(path: "document.txt")
+                
+                do {
+                    try data.write(to: url, options: [.atomic, .completeFileProtection])
+                    
+                    let readData = try String(contentsOf: url, encoding: .utf8)
+                    print(readData)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
         }
         .padding()
     }
