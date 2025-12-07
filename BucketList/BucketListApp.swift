8cac0b58b7267d765129628c5f93c2f29b4ce6c9
asi-isa <ali.inceoglu@protonmail.com>
2025-12-07 22:13:54 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Ali Soner Inceoglu on 07.12.25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
